0b72e9d7b495bb316a674ef01fbc1119b3b36f9e
Ricardo Santos <ricardojoaosantos0@gmail.com>
2018-09-13 17:20:29 +0100

    Env firebase config

---
+++ src/environment/environment.ts
@@ -0,0 +1,10 @@
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAccj22fp2O6NtoYnzc4s0K5nZJbwIcsYQ",
+    authDomain: "wishbox-671f0.firebaseapp.com",
+    databaseURL: "https://wishbox-671f0.firebaseio.com",
+    projectId: "wishbox-671f0",
+    storageBucket: "wishbox-671f0.appspot.com",
+    messagingSenderId: "136609741038"
+  };
